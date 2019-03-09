cfb0ab30052b5ff9846eb98bab0885cab540f089
LucasLombardo <lucasalombardo@gmail.com>
2019-03-09 13:06:37 -0500

    add get request

--- src/components/books/readBooks.js
+++ src/components/books/readBooks.js
@@ -2,9 +2,22 @@ import React, { useState, useEffect } from "react"
 import axios from "axios"
 
 const readBooks = () => {
+  const [ books, setBooks ] = useState([])
+
+  const getBooks = () => {
+    axios.get(`http://localhost:4741/books`)
+    .then(res => {
+      // setBooks(res.data.books)
+      console.log(res.data)
+    })
+  }
+
   return (
     <div>
-      <button>read all</button>
+      <button onClick={getBooks}>read all</button>
+      {/* <ul style={{display: 'inline'}}>
+        {books.map(book => <li>{book.title}</li>)}
+      </ul> */}
     </div>
   )
 }
